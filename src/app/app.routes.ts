185f30a879e318b763c5b35dce881f04d4df9291
juanfar <juanfar.group@gmail.com>
2025-08-14 20:48:43 +0200

    chore: Routes and Http modules activated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  {
+    path: 'home',
+    data: { breadcrumb: 'Inicio' },
+    loadComponent: () =>
+      import('./features/home/home.component').then(m => m.HomeComponent),
+    children: [
+      // TO DO: ruta hija por defecto (opcional pero recomendable)
+
+      {
+        path: 'invoices',
+        data: { breadcrumb: 'Facturas' },
+        loadComponent: () =>
+          import('./features/invoices/invoices.component').then(m => m.InvoicesComponent),
+      },
+      {
+        path: 'accounts',
+        data: { breadcrumb: 'Cuentas' },
+        loadComponent: () =>
+          import('./features/accounts/accounts.component').then(m => m.AccountsComponent),
+      },
+      {
+        path: 'accounts/:id',
+        data: { breadcrumb: 'Cuenta Detalle' },
+        loadComponent: () =>
+          import('./features/accounts/accounts.component').then(m => m.AccountsComponent),
+      },
+      {
+        path: 'profile',
+        data: { breadcrumb: 'Perfil' },
+        loadComponent: () =>
+          import('./features/profile/profile.component').then(m => m.ProfileComponent),
+      },
+      {
+        path: 'settings',
+        data: { breadcrumb: 'Configuración' },
+        loadComponent: () =>
+          import('./features/settings/settings.component').then(m => m.SettingsComponent),
+      },
+    ],
+  },
+];
