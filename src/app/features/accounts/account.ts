ec01781aa854bcdf8ac334121e151f47ab8124a9
juanfar <juanfar.group@gmail.com>
2025-08-17 22:57:30 +0200

    feat: Account model new properties

--- src/app/features/accounts/account.ts
+++ src/app/features/accounts/account.ts
@@ -1,10 +1,14 @@
 export interface Account {
   account_id?: string;
+  account_id_short?: string;
   avatar?: string;
   name: string;
+  alias: string;
   status: boolean;
   last_invoice_date?: string;
   job_role?: string;
+  cif?: string;
+  address?: string;
 }
 export interface AccountList {
   accounts: Account[];
@@ -16,22 +20,28 @@ export const mockAccounts: AccountList = {
       account_id: '01J8P3N6',
       avatar: 'https://api.midominio.com/avatar?name=Juan%20Perez',
       name: 'Account One',
+      alias: 'Cuenta de Juan1',
       status: true,
       last_invoice_date: '2023-05-01',
+      job_role: 'Desarrollador Backend',
     },
     {
       account_id: '5B98B09A',
       avatar: 'https://api.midominio.com/avatar?name=Juan%20Perez',
       name: 'Account Two',
+      alias: 'Cuenta de Juan2',
       status: false,
       last_invoice_date: '2023-05-01',
+      job_role: 'Desarrollador Backend',
     },
     {
       account_id: '7CA71512',
       avatar: 'https://api.midominio.com/avatar?name=Juan%20Perez',
       name: 'Account Three',
+      alias: 'Cuenta de Juan3',
       status: true,
       last_invoice_date: '2023-05-01',
+      job_role: 'Desarrollador Backend',
     }
   ]
 };
