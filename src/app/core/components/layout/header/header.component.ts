bc38b4d306cb26f3a483c3fbfdb3d7850bc1e24c
juanfar <juanfar.group@gmail.com>
2025-08-21 21:52:50 +0200

    feat: Signout functionality created

--- src/app/core/components/layout/header/header.component.ts
+++ src/app/core/components/layout/header/header.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, FontAwesomeModule],
+  imports: [RouterModule, FontAwesomeModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  private router = inject(Router);
+  private authService = inject(AuthService);
   faGear = faGear;
   faUser = faUser;
   faCircleUser = faCircleUser;
   faRightFromBracket = faRightFromBracket;
+
+  async signOut() {
+    try {
+      await this.authService.signout();
+      localStorage.removeItem('user');
+      localStorage.removeItem('session');
+      this.router.navigate(['/auth/signin']);
+    } catch (error) {
+      console.error('Error during sign out:', error);
+    }
+  }
 }
